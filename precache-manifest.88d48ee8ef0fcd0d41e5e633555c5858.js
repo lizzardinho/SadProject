5067418a4fc446334832577967d761ce314e2649
lizzardinho <banjolieb@gmail.com>
2022-09-22 17:55:38 +0200

    firstcommit

---
+++ precache-manifest.88d48ee8ef0fcd0d41e5e633555c5858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "25f07eddbf09d1dd241a",
+    "url": "/static/css/main.36ff7d64.chunk.css"
+  },
+  {
+    "revision": "25f07eddbf09d1dd241a",
+    "url": "/static/js/main.25f07edd.chunk.js"
+  },
+  {
+    "revision": "e8336ac29837ab60df49",
+    "url": "/static/js/1.e8336ac2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e6c1fb58c63de23581f3fa90de815ef5",
+    "url": "/static/media/4.e6c1fb58.png"
+  },
+  {
+    "revision": "80f07f6e28e66a2e437d9a8cb2b125d9",
+    "url": "/static/media/T7HT.80f07f6e.gif"
+  },
+  {
+    "revision": "72c7aedbb461812f7a6f19833dfdffb3",
+    "url": "/index.html"
+  }
+];
